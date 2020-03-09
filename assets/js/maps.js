004f21209dd723bcb94105fc13a7d9edb517066f
Sebastian-Torres-Matrix <sebastian_torres@hotmail.se>
2020-03-09 19:10:43 +0000

    fixed maps.js now working

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,20 +1,19 @@
-
-
-    function initMap() {
+function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 3,
           center: {
-              lat: -28.024, 
-              lng: 140.887}
+              lat: 41.390205, 
+              lng: 2.154007
+            }
         });
 
          var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-         var locations = {
-            {lat: -31.563910, lng: 147.154312},
-            {lat: -33.718234, lng: 150.363181},
-            {lat: -33.727111, lng: 150.371124}
-         };
+         var locations = [
+            {lat: 41.380898, lng: 2.122820},
+            {lat: 41.403706, lng: 2.173504},
+            {lat: 41.380894, lng: 2.189385}
+         ];
 
         var markers = locations.map(function(location, i) {
           return new google.maps.Marker({
@@ -22,4 +21,6 @@
             label: labels[i % labels.length]
           });
         });
-        
+    
+        var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+    }
