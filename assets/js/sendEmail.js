743b36f8694ea919f01da24fa7cf50f4d1a1cf07
Sebastian-Torres-Matrix <sebastian_torres@hotmail.se>
2020-04-06 17:45:49 +0000

    comments added in all .html files

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,15 +1,21 @@
+
+// Creates email service 
 function sendMail(contactForm) {
     emailjs.send("gmail", "code_institute", {
+        // values to input in the form
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "message": contactForm.messagesummary.value
     })
         .then(
+            // Confirms in the console if submit was succesful
             function (response) {
                 console.log("SUCCESS", response);
             },
+            // Confirms in the console if submit was unsuccesful
             function (error) {
                 console.log("FAILED", error)
             })
+    // blocks loading a new page
     return false;
 }
